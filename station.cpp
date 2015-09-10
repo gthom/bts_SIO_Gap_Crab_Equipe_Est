538885aeda112111ce4e1a760364044db7174fd7
rmagat <rmagat@f005ThinkStation13.btsinfogap.org>
2015-09-10 11:45:36 +0200

    Ajout du void de la visiteAFaire sur station.cpp

--- station.cpp
+++ station.cpp
@@ -20,5 +20,18 @@ Station::Station(int s, QString l)
 
 void getVisiteAFaire()
 {
-
+    //retourne une instance de classe Visite recensant toutes les bornes de la station qui nécessitent d'être révisées,
+    //ou null s'il n'y a aucune borne à réviser
+    Visite laVisiteAFaire;
+    //pour chaque borne du vecteur lesBornes
+    foreach(int idBorne, lesBornes)
+    {
+        //si la borne est à visiter je l'ajoute à laVisiteAFaire
+        if(idBorne == p)
+        {
+            laVisiteAFaire=idBorne;
+            //je renvoie laVisiteAFaire
+            return laVisiteAFaire;
+        }
+    }
 }
