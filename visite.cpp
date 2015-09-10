9587118524ee7163a781eec83b132d808e125c11
Martin-Reymond <leeroy.martin.jenkins@gmail.com>
2015-09-10 10:43:50 +0200

    Ajout réel de la classe Visite

---
+++ visite.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file visite.cpp
+ * @date 10 septembre 2015
+ * @brief travail sur les visites à une borne
+ * @author Martin Reymond
+ * @version
+ */
+
+#include "visite.h"
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+Visite::Visite()
+{
+}
+Visite::Visite(char e, int d, Station ls, vector<Borne> tb)
+{
+	etat=e;
+	dureeTotale=d;
+	laStation=ls;
+	tabBorne=tb;
+}
+// retourne la durée totale en minutes requise pour réaliser l'ensemble 
+// des révisions prévues sur les bornes de la station
+int Visite::getDureeTotale()
+{
+	return dureeTotale;
+}
+// retourne l'état de la visite
+char Visite::getEtat()
+{
+	return etat
+}
+// modifie l'état de la visite, de 'p' programmée à 'a' affectée, ou de 'a' affectée à 'r' réalisée
+void Visite::changerEtat()
+{
+	if(etat == p)
+	{
+		etat==a;
+	}
+	if(etat == a)
+	{
+		etat==r;
+	}
+	else
+	{
+		return 0;
+	}
+	
+}
+
+
---
+++ visite.h
@@ -0,0 +1,40 @@
+/**
+ * @file visite.h
+ * @date 10 septembre 2015
+ * @brief travail sur les visites à une borne
+ * @author Martin Reymond
+ * @version
+ */
+
+#include <string>
+#include <vector>
+#include "borne.h"
+#include "station.h"
+using namespace std;
+//déclaration
+class Visite
+{
+	private :
+		//état de la visite : 'p' pour programmée, 'a' pour affectée, 'r' pour réalisée
+		char etat;
+		// durée totale en minutes requise pour réaliser l'ensemble des révisions 
+		// prévues sur les bornes de la station
+		int dureeTotale;
+		// la station concernée par la visite		
+		Station laStation; 
+		// la collection des bornes de laStation concernées par la visite
+		vector<Borne> tabBorne; 
+	public :
+		
+		// constructeur de Visite
+		Visite(char e, int d, Station ls, vector<Borne> tb);
+		Visite();
+		// retourne la durée totale en minutes requise pour réaliser l'ensemble
+		// des révisions prévues sur les bornes de la station
+		int getDureeTotale();
+		// retourne l'état de la visite
+		char getEtat();
+		// retourne l'état de la visite
+		void changerEtat();		
+
+};
