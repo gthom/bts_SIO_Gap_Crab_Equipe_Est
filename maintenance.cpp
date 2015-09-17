e028366135a4bad6c2dec0574848d6fb9d614301
RChauveau <rm.chauveau@gmail.com>
2015-09-17 11:58:58 +0200

    Modification de la classe maintenance

--- maintenance.cpp
+++ maintenance.cpp
@@ -1,5 +1,22 @@
 #include "maintenance.h"
 
+Maintenance::Maintenance(vector<Stations> ls, vector<Techniciens> lt, vector<Visites> lv)
+{
+    lesStations = ls;
+    lesTechniciens = lt;
+    lesVisites = lv;
+}
+
 Maintenance::Maintenance()
 {
+}
+
+void Maintenance::reviser()
+{
+
+}
+
+void Maintenance::affecterVisites()
+{
+
 }
