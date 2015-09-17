f2221c79bbc7b8f868de0d008516d9e57c538efe
RChauveau <rm.chauveau@gmail.com>
2015-09-17 14:37:44 +0200

    Ajout de commentaire

--- maintenance.cpp
+++ maintenance.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <vector>
 
+/**
+ * @brief Constructeur de la classe maintenance
+ */
 Maintenance::Maintenance(vector<Station> ls, vector<Technicien> lt, vector<Visite> lv)
 {
     lesStations = ls;
@@ -9,15 +12,26 @@ Maintenance::Maintenance(vector<Station> ls, vector<Technicien> lt, vector<Visit
     lesVisites = lv;
 }
 
+/**
+ * @brief Constructeur vide
+ */
 Maintenance::Maintenance()
 {
 }
 
+/**
+ * @brief Etablit l'ensemble des visites à réaliser sur les stations
+ */
 void Maintenance::reviser()
 {
 
 }
 
+/**
+ * @brief Affecte les visites à réaliser aux techniciens, en répartissant équitablement le travail
+ * entre les techniciens. Chaque visite est affectée au technicien le moins occupé en temps
+ * total de maintenance préventive. L'état de chaque visite doit alors être mis à jour.
+ */
 void Maintenance::affecterVisites()
 {
 
--- maintenance.h
+++ maintenance.h
@@ -1,10 +1,9 @@
 /**
- * @author Robin Lesieur
+ * @author Robin Lesieur, Rémi Chauveau
  * @file maintenance.h
- * @date 10/08/15
+ * @date 17/08/15
  * @class Maintenance
  */
-
 
 #ifndef MAINTENANCE_H
 #define MAINTENANCE_H
@@ -15,6 +14,22 @@
 #include <QString>
 #include <vector>
 
+/**
+ * @class Classe Maintenance
+ * @brief Classe représentant la maintenance
+ * Private :
+ * vector<Station> lesStations;
+ * vector<Technicien> lesTechniciens;
+ * vector<Visite> lesVisites;
+ * Public :
+ * constructeur maintenance
+ * @fn reviser()
+ * @brief Etablit l'ensemble des visites à réaliser sur les stations
+ * @fn affecterVisites ()
+ * @brief Affecte les visites à réaliser aux techniciens, en répartissant équitablement le travail
+ * entre les techniciens. Chaque visite est affectée au technicien le moins occupé en temps
+ * total de maintenance préventive. L'état de chaque visite doit alors être mis à jour.
+ */
 class Maintenance
 {
 private:
