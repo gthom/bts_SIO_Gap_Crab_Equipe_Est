7a69a9152b38866263fa4f0e4f3d6ef53165e809
RChauveau <rm.chauveau@gmail.com>
2015-09-17 15:40:30 +0200

    commentaire

--- technicien.cpp
+++ technicien.cpp
@@ -21,13 +21,13 @@ Technicien::Technicien(int m, QString n, QString p, vector<Visite> lv)
 
     //requête qui va chercher les infos des Technicien dans la bdd
     //et les ajoute à la visite selectionnée
-    QSqlQuery query;
-    query.exec("SELECT * FROM TECHNICIEN WHERE id = '" + QString::number(lesVisites) + "'");
-    //tant que la requete envoi un résultats
-    while(query.next())
-    {
+    //QSqlQuery query;
+    //query.exec("SELECT * FROM TECHNICIEN WHERE id = '" + QString::number(lesVisites) + "'");
+    //tant que la requete renvoi un résultats
+    //while(query.next())
+    //{
         //QString idBorne = query.value(0).toInt();
-    }
+    //}
 	
 }
 /**
