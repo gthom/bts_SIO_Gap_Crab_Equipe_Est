d3faa22ff98c2eb603d61333b5dd6f4302e37615
Pitt05 <pierre.bouffier05@gmail.com>
2015-09-17 15:37:15 +0200

    Avancement

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,20 @@
+/**
+ * @author Pierre Bouffier
+ * @version 1.0.0
+ * @file mainwindow.cpp
+ */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QSqlTableModel>
 #include <QSqlQuery>
 
+
+/**
+ * @brief MainWindow::MainWindow
+ * @param parent
+ * Construit le mainwindow principal.
+ */
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,10 +23,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/**
+ * @brief MainWindow::~MainWindow
+ * Déconstruit le mainwindow principal.
+ */
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+/**
+ * @brief MainWindow::on_pushButtonConnect_clicked
+ * Evènement permettant au programme de lister toute les station dans un table view.
+ */
 
 void MainWindow::on_pushButtonConnect_clicked()
 {
@@ -27,13 +49,19 @@ void MainWindow::on_pushButtonConnect_clicked()
     query.exec("SELECT * FROM STATION");
     while(query.next())
     {
-        Station nouvelleStation = new Station(query.value(0).toInt(), query.value(1).toString())
+        Station* nouvelleStation = new Station(query.value(0).toInt(), query.value(1).toString());
     }
 
     ui->tableViewStation->setModel(model);
 }
 
-void MainWindow::on_action_Quitter_triggered()
+/**
+ * @brief MainWindow::on_tableViewStation_clicked
+ * @param index
+ * Evènement qui permet à l'index de la ligne d'être sélectionner afin de lister les bornes.
+ */
+
+void MainWindow::on_tableViewStation_clicked(const QModelIndex &index)
 {
-    close();
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -19,6 +19,8 @@ public:
 private slots:
     void on_pushButtonConnect_clicked();
 
+    void on_tableViewStation_clicked(const QModelIndex &index);
+
 private:
     Ui::MainWindow *ui;
 
